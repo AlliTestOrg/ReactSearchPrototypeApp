c8ad6c86d3d7f0548ab54cba11a9e079d144bbfb
heylookalive <allister.price@gmail.com>
2016-10-05 20:43:17 +0100

    Correction for api tests not running.

--- tests/api/run-tests.sh
+++ tests/api/run-tests.sh
@@ -3,7 +3,9 @@
 # Download silk if not here.
 if [ ! -f ./silk ]; then
   wget https://github.com/matryer/silk/releases/download/0.5.1/silk-0.5.1-linux-amd64.zip
-  tar -xzf silk-0.5.1-linux-amd64.zip
+  unzip silk-0.5.1-linux-amd64.zip
+  mv silk-0.5.1-linux-amd64/silk .
+  rm -rf silk-0.5.1-linux-amd64.zip silk-0.5.1-linux-amd64
   chmod a+x silk
 fi
 
---
+++ tests/api/tests/0-check-platform.md
@@ -0,0 +1,10 @@
+# Checking the platform is up
+
+## GET /
+Checks that the platform is up and spitting out the standard JSON response.
+
+---
+* Status: 200
+* Content-Type: "application/json; charset=UTF-8"
+* Data.tagline: "You Know, for Search"
+* Data.version.number: "2.3.4"
